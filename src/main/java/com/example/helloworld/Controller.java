1761da99787c19a5de14c0cd89034d565ded3065
马克 <hackk@163.com>
2022-12-19 00:36:23 +0800

    第一次提交

---
+++ src/main/java/com/example/helloworld/Controller.java
@@ -0,0 +1,16 @@
+package com.example.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 马克
+ * @date 2022/12/19
+ */
+@RestController
+public class Controller {
+    @GetMapping("hello")
+    public String sayHello() {
+        return "Hello World";
+    }
+}
