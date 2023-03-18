c2056a22b607941073e876f1b1291262e8357894
马克 <hackk@163.com>
2023-03-18 10:08:21 +0800

    更新返回值

--- src/main/java/com/example/helloworld/Controller.java
+++ src/main/java/com/example/helloworld/Controller.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("hello")
     public String sayHello() {
-        return "Hello World";
+        return "Hello World~~";
     }
 }
